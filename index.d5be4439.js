37baeae2813c70f6cb060dd35aa9f55521bb7559
Illia Safonov <safonov.illia@gmail.com>
2026-01-21 16:19:13 +0100

    make build

---
+++ index.d5be4439.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var r=e.querySelectorAll("th, td"),t=r[1].cloneNode(!0),l=r[r.length-1];e.insertBefore(t,l)});
+//# sourceMappingURL=index.d5be4439.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const rows = document.querySelectorAll("tr");
+rows.forEach((row)=>{
+    const cells = row.querySelectorAll("th, td");
+    const targetCell = cells[1].cloneNode(true);
+    const lastCell = cells[cells.length - 1];
+    row.insertBefore(targetCell, lastCell);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
